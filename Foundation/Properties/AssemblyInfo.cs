06415e216428b0a1ddb0927ad00c5f25baf65e7b
ThomasHolmes_cp <dd9ae165a9772f92a50f13bdeb1fe6cc6ac1579fhYedqQbG>
2011-07-07 16:33:50 +0000

    Corrected assembly info.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS06]$/51degrees;C60339

--- Foundation/Properties/AssemblyInfo.cs
+++ Foundation/Properties/AssemblyInfo.cs
@@ -39,8 +39,8 @@ using System.Security;
 //      Revision
 //
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.0.5")]
+[assembly: AssemblyFileVersion("1.2.0.5")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 [assembly: AllowPartiallyTrustedCallers]
 
