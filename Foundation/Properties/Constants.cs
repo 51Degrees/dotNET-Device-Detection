943bc7de0dae8d87291c0d604485ddc01620eb1a
ThomasHolmes_cp <dd9ae165a9772f92a50f13bdeb1fe6cc6ac1579fhYedqQbG>
2011-12-05 09:33:17 +0000

    Added constants.cs file
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS06]$/51degrees;C63904

---
+++ Foundation/Properties/Constants.cs
@@ -0,0 +1,38 @@
+﻿/* *********************************************************************
+ * The contents of this file are subject to the Mozilla Public License 
+ * Version 1.1 (the "License"); you may not use this file except in 
+ * compliance with the License. You may obtain a copy of the License at 
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing rights and 
+ * limitations under the License.
+ *
+ * The Original Code is named .NET Mobile API, first released under 
+ * this licence on 11th March 2009.
+ * 
+ * The Initial Developer of the Original Code is owned by 
+ * 51 Degrees Mobile Experts Limited. Portions created by 51 Degrees 
+ * Mobile Experts Limited are Copyright (C) 2009 - 2011. All Rights Reserved.
+ * 
+ * Contributor(s):
+ *     James Rosewell <james@51degrees.mobi>
+ * 
+ * ********************************************************************* */
+
+namespace FiftyOne.Foundation.Mobile
+{
+    internal static class Constants
+    {
+        /// <summary>
+        /// Alternative configuration file name
+        /// </summary>
+        internal const string CONFIG_FILENAME = "~/App_Data/51Degrees.mobi.config";
+
+        /// <summary>
+        /// Name for Azure cloud storage
+        /// </summary>
+        internal const string AZURE_STORAGE_NAME = "fiftyonedegrees";
+    }
+}
