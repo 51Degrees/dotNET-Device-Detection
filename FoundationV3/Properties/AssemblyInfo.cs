1c22cc423a8978e6f8d1decb30666261c354977a
jwrosewell_cp <15dfd780c98c793c20f914b9953506281aef9b3cwUtvyney>
2014-08-13 13:05:43 +0000

    Increased version number for the copyright message change.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS06]$/51degrees;C75244

--- FoundationV3/Properties/AssemblyInfo.cs
+++ FoundationV3/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Security;
 //      Build Number
 //      Revision
 
-[assembly: AssemblyVersion("3.1.5.3")]
-[assembly: AssemblyFileVersion("3.1.5.3")]
+[assembly: AssemblyVersion("3.1.5.4")]
+[assembly: AssemblyFileVersion("3.1.5.4")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 [assembly: AllowPartiallyTrustedCallers]
