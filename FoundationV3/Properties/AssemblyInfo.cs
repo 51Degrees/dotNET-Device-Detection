248e837a11af45b2eab4c7733b2f8b2d2d58aeaa
ThomasHolmes_cp <dd9ae165a9772f92a50f13bdeb1fe6cc6ac1579fhYedqQbG>
2014-09-18 09:38:19 +0000

    Updated version number.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS06]$/51degrees;C75441

--- FoundationV3/Properties/AssemblyInfo.cs
+++ FoundationV3/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Security;
 //      Build Number
 //      Revision
 
-[assembly: AssemblyVersion("3.1.6.1")]
-[assembly: AssemblyFileVersion("3.1.6.1")]
+[assembly: AssemblyVersion("3.1.6.6")]
+[assembly: AssemblyFileVersion("3.1.6.6")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 [assembly: AllowPartiallyTrustedCallers]
