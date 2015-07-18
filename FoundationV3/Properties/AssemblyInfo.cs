7bac5cd70133af71342a8c56c41c068ee5e4f93e
James Rosewell <james@51degrees.com>
2015-07-18 03:50:28 +0100

    New version number

--- FoundationV3/Properties/AssemblyInfo.cs
+++ FoundationV3/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Security;
 //      Build Number
 //      Revision
 
-[assembly: AssemblyVersion("3.2.3.1")]
-[assembly: AssemblyFileVersion("3.2.3.1")]
+[assembly: AssemblyVersion("3.2.3.2")]
+[assembly: AssemblyFileVersion("3.2.3.2")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 [assembly: AllowPartiallyTrustedCallers]
