a3787680489d5ec902e979458ecf7a5947158c65
James Rosewell <james@51degrees.com>
2015-12-15 18:25:37 +0000

    EXAMPLE: Included MVC example for tutorials.
    Added an example to demonstrate how device detection results can be accessed in MVC views and controllers. Meta data and property values for the request are all demonstrated.
    
    Removed 51Degrees redirect section from 51Degrees.config as not related to MVC.

---
+++ Examples/MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FiftyOne.Foundation.Mobile.Detection;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            // Sets the title for the Home page.
+            ViewBag.Title = "ASP.NET MVC Device Detection Example";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            // Sets title and message for the About page.
+            ViewBag.Title = "About This Example";
+            ViewBag.Message = "51Degrees MVC example description page.";
+
+            return View();
+        }
+    }
+}
