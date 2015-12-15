5e775abbf4734801fc34c861faf2ef1275908cb1
James Rosewell <james@51degrees.com>
2015-12-15 18:25:37 +0000

    EXAMPLE: Included MVC example for tutorials.
    Added an example to demonstrate how device detection results can be accessed in MVC views and controllers. Meta data and property values for the request are all demonstrated.
    
    Removed 51Degrees redirect section from 51Degrees.config as not related to MVC.
    
    
    Former-commit-id: 1c1b25d3541446957fb3d8c1ea4441beabca4a91

---
+++ Examples/MVC/Controllers/BaseController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FiftyOne.Foundation.Mobile.Detection;
+using MVC.Models;
+using System.Web.Routing;
+
+namespace MVC.Controllers
+{
+    // Snippet Start
+    public class BaseController : Controller
+    {
+        /// <summary>
+        /// If there is an active device detection provider then use this to
+        /// add dynamic properties to the ViewBag. 
+        /// </summary>
+        /// <para>
+        /// The Device property of the ViewBag is set to a new MVC model 
+        /// instance which copies properties from the device match result.
+        /// </para>
+        /// <para>
+        /// The Match propety of the ViewBag is set to directly expose the
+        /// match instance returned from device detection.
+        /// </para>
+        /// <param name="requestContext"></param>
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            
+            if (WebProvider.ActiveProvider != null)
+            {
+                // Perform device detection on the headers provided in the
+                // request.
+                var match = WebProvider.ActiveProvider.Match(
+                    requestContext.HttpContext.Request.Headers);
+                
+                // Create a model that is based on the match request from
+                // device detection.
+                ViewBag.Device = new Device(match);
+
+                // Also expose the match result directly in the ViewBag
+                // to compare the different access methods when used in the 
+                // view.
+                ViewBag.Match = match;
+            }
+
+            // Get the HTTP headers from the request to display their values in
+            // the view.
+            ViewBag.RequestHeaders =
+                requestContext.HttpContext.Request.Headers;
+        } 
+    }
+    // Snippet End
+}
+
---
+++ Examples/MVC/Models/Device.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using FiftyOne.Foundation.Mobile.Detection;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Device
+    {
+        /// <summary>
+        /// HTML used if the property has no value.
+        /// </summary>
+        private const string SWITCH_HTML = "<a href=\"" +
+            "https://51degrees.com/compare-data-options\">" +
+            "Switch Data Set</a>";
+
+        // Snippet Start
+        /// <summary>
+        /// Instance of a device detection match to use for device properties.
+        /// </summary>
+        private readonly Match _match;
+
+        // These properties are fetched only when requested by the view. This 
+        // can be a more efficient method of accessing property values as 
+        // they're only retrieved when requested.
+        public bool IsMobile 
+        { 
+            get 
+            { 
+                return _match["IsMobile"] != null ? 
+                    _match["IsMobile"].ToBool() : 
+                    false; 
+            } 
+        }
+        public int ScreenPixelsHeight 
+        { 
+            get 
+            { 
+                return _match["ScreenPixelsHeight"] != null ?
+                    (int)_match["ScreenPixelsHeight"].ToDouble() :
+                    0; 
+            } 
+        }
+        public int ScreenPixelsWidth
+        {
+            get
+            {
+                return _match["ScreenPixelsWidth"] != null ?
+                    (int)_match["ScreenPixelsWidth"].ToDouble() :
+                    0;
+            }
+        }
+
+        // These properties are populated in the constructor with
+        // 51Degrees properties of the same name. More can be added
+        // as long as their name is the same as the 51Degrees
+        // property and are string types. More properties
+        // are listed at https://51degrees.com/Resources/Property-Dictionary
+        public string HardwareVendor { get; private set; }
+        public string HardwareModel { get; private set; }
+        public string PlatformVendor { get; private set; }
+        public string PlatformName { get; private set; }
+        public string PlatformVersion { get; private set; }
+        public string BrowserVendor { get; private set; }
+        public string BrowserName { get; private set; }
+        public string BrowserVersion { get; private set; }
+        
+        /// <summary>
+        /// Uses reflection to see if there is a 51Degrees property with the 
+        /// same name as a Device class property. This means getting a new 
+        /// 51Degrees property in this object only requires creating a 
+        /// property with that name.
+        /// </summary>
+        /// <param name="match">Instance of a device detection match</param>
+        internal Device(Match match)
+        {
+            _match = match;
+            foreach (var classProperty in this.GetType().GetProperties().Where(p => 
+                p.PropertyType == typeof(string)))
+            {
+                var values = match[classProperty.Name];
+                if (values != null && values.Count > 0)
+                {
+                    // There is a value for the property. Set the value
+                    // now.
+                    classProperty.SetValue(this, values.ToString());
+                }
+                else
+                {
+                    // Property is not contained in the active 51Degrees
+                    // data set. Display a link to switch the data set
+                    // and re-run the example.
+                    classProperty.SetValue(this, SWITCH_HTML);
+                }
+            }
+        }
+        // Snippet End
+    }
+}
