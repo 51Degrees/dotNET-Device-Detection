afee11cac88889c7e9d03093b5d776e6c4f06a23
Ben Shillito <ben@51degrees.com>
2016-09-13 11:03:50 +0100

    EXAMPLE/BUILD: Added all profiles example, and removed unnecessary configurations.
    The all profiles example fetches all the hardware profiles, then outputs all the devices and their properties to a CSV file. Two filters are applied to the properties that are printed: firstly only hardware properties are fetched as they are the only properties that relate to a hardware profiles, also any javascript property is given the value "N/A" as these can return long multiline javascript which will ruin any CSV file formatting.
    
    In the same way as with the C repository, all configurations other than x64 have been removed as they are not needed and just make maintaing and adding projects more complex.
    
    
    Former-commit-id: b24b87c17af537a8c157b165e7fdfc3f913e41d6

---
+++ Examples Tests/AllProfiles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FiftyOne.Example.Illustration.AllProfiles;
+using FiftyOne.Tests.Integration;
+
+namespace FiftyOne.Tests.Example
+{
+    [TestClass]
+    public class AllProfiles
+    {
+        [TestMethod]
+        [TestCategory("Example"), TestCategory("Unit")]
+        public void LiteExamples_All_Profiles()
+        {
+            Program.Run(
+                Utils.GetDataFile(Constants.LITE_PATTERN_V32));
+        }
+        [TestMethod]
+        [TestCategory("Example"), TestCategory("Unit")]
+        public void PremiumExamples_All_Profiles()
+        {
+            Program.Run(
+                Utils.GetDataFile(Constants.PREMIUM_PATTERN_V32));
+        }
+        [TestMethod]
+        [TestCategory("Example"), TestCategory("Unit")]
+        public void EnterpriseExamples_All_Profiles()
+        {
+            Program.Run(
+                Utils.GetDataFile(Constants.ENTERPRISE_PATTERN_V32));
+        }
+    }
+}
