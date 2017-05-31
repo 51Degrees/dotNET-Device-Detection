0fc9c6a5532dd98bdbc8253267bfb4db03797987
Ben51Degrees <ben@51degrees.com>
2017-05-31 12:24:42 +0100

    DOC: Added class summary to SerializationAttribute class.
    Former-commit-id: e790689d671e0803b98b2ac3a2520286293cd28f

--- FoundationV3/SerializableAttribute.cs
+++ FoundationV3/SerializableAttribute.cs
@@ -1,6 +1,9 @@
 ﻿#if !NET40
 namespace System.Runtime.Serialization
 {
+    /// <summary>
+    /// Required for .NET Standard build to work.
+    /// </summary>
     public class SerializableAttribute : Attribute
     {
     }
